59f63e91af706c97ac86ae445998834d01600dfb
bdebiase <debiase2@gmail.com>
2023-11-14 11:22:10 -0500

    Update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -3,60 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix.url = "github:nix-community/fenix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlays.default ];
         };
       in
       {
         devShell = pkgs.mkShell {
-          shellHook = ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
-              pkgs.alsaLib
-              pkgs.udev
-              pkgs.vulkan-loader
-            ]}"
-          '';
+	  nativeBuildInputs = [
+	    pkgs.pkg-config
+	    pkgs.clang
+	    pkgs.lld
+	    pkgs.mold
+	  ];
 
           buildInputs = with pkgs; [
-            (
-              with fenix.packages.${system};
-              combine (
-                with default; [
-                  cargo
-                  clippy-preview
-                  latest.rust-src
-                  rust-analyzer
-                  rust-std
-                  rustc
-                  rustfmt-preview
-                ]
-              )
-            )
-            cargo-edit
-            cargo-watch
-            lld
-            clang
-            pkg-config
-            udev
-            alsaLib
-            lutris
-	          xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            vulkan-tools
-            vulkan-headers
-            vulkan-loader
-            vulkan-validation-layers
-          ];
+	    cargo
+	    rustc
+	    rustfmt
+	    pre-commit
+	    rustPackages.clippy
+	    alsa-lib
+	    udev
+	    vulkan-loader
+	    xorg.libX11
+	    xorg.libXrandr
+	    xorg.libXcursor
+	    xorg.libXi
+	  ];
+
+	  shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+	      pkgs.lib.makeLibraryPath [
+                pkgs.alsaLib
+                pkgs.udev
+                pkgs.vulkan-loader
+              ]}"
+          '';
+	  RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
       }
     );
